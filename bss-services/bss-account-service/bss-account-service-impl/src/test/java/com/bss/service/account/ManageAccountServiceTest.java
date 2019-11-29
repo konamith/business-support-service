6f87dfa7acde62d924bf6c4b4282eea3de4705eb
liuming <robin3r@126.com>
2019-11-29 21:53:24 +0800

    增加了测试依赖

---
+++ bss-services/bss-account-service/bss-account-service-impl/src/test/java/com/bss/service/account/ManageAccountServiceTest.java
@@ -0,0 +1,5 @@
+package com.bss.service.account;
+
+public class ManageAccountServiceTest {
+
+}
---
+++ bss-test/src/main/java/com/bss/test/account/ManageAccountServiceTest.java
@@ -0,0 +1,9 @@
+package com.bss.test.account;
+
+import com.alibaba.dubbo.config.annotation.Service;
+
+@Service
+public class ManageAccountServiceTest {
+
+
+}
