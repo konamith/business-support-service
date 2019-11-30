9f099a754411209a77f7c1a46c65c8d6acf77db3
liuming <robin3r@126.com>
2019-11-30 17:15:37 +0800

    项目启动类改进

--- bss-services/bss-account-service/bss-account-service-impl/src/main/java/com/bss/service/account/application/AccountServiceApplication.java
+++ bss-services/bss-account-service/bss-account-service-impl/src/main/java/com/bss/service/account/application/AccountServiceApplication.java
@@ -1,12 +1,17 @@
 package com.bss.service.account.application;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bss.service.account")
+@EnableDubbo(scanBasePackages = "com.bss.service.account")
+@PropertySources({@PropertySource("classpath:bss-account-service.properties")})
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceConfig.class);
+        SpringApplication.run(AccountServiceApplication.class);
     }
 }
--- bss-services/bss-account-service/bss-account-service-impl/src/test/java/com/bss/service/account/ManageAccountServiceTest.java
+++ bss-services/bss-account-service/bss-account-service-impl/src/test/java/com/bss/service/account/ManageAccountServiceTest.java
@@ -1,5 +1,12 @@
 package com.bss.service.account;
 
+import com.bss.service.account.application.AccountServiceApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = AccountServiceApplication.class)
 public class ManageAccountServiceTest {
 
 }
